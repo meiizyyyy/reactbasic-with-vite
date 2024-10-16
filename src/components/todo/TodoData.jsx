6bf9c1cd4cd2f69c90202f766e7470c6904572b3
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2024-10-17 00:10:52 +0700

    #33 TodoComponents

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+const TodoData = () => {
+    return (
+        <>
+            <div className="todo-list">
+                <div>Learning React</div>
+                <div>Watching Youtube</div>
+            </div>
+        </>
+    );
+};
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,12 @@
+const TodoNew = () => {
+    return (
+        <>
+            <div className="todo-input">
+                <input type="text" />
+                <button>Add</button>
+            </div>
+        </>
+    );
+};
+
+export default TodoNew;
