742862d22495e75ed0738a8156aa56ec38d11e7c
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2024-10-17 00:57:27 +0700

    #34 props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+    const name = "meiizyyyy";
+    const age = "21";
+    const data = {
+        address: "Son Tay",
+        country: "Viet Nam",
+    };
+    // {key:value} => object
+
     return (
         <>
             <div className="todo-container">
                 <div className="todo-title">Todo List</div>
                 <TodoNew />
-                <TodoData />
+                {/* Truyền dữ liệu từ cha sang con */}
+                <TodoData name={name} age={age} data={data} address={data.address} />
                 <div className="todo-image">
                     <img src={reactLogo} className="logo" />
                 </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,17 @@
-const TodoData = () => {
+const TodoData = props => {
+    //props = object
+    //use destructuring
+
+    const { name, age, data } = props;
+    //có thể hiểu dòng trên là:
+    // const name = props.name;
+    // const age = props.age;
+
+    console.log("check: ", props);
     return (
         <>
             <div className="todo-list">
+                <div>My name is {name}</div>
                 <div>Learning React</div>
                 <div>Watching Youtube</div>
             </div>
