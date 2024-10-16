2c4471df2c628f3a2349f4ecc4e79b33b44d24d4
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2024-10-16 20:28:07 +0700

    #31 truyen bien so vao JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,20 @@
 import "./style.css";
 
 const MyComponent = () => {
+    // const meiizyyyy = "React";
+    // const meiizyyyy = 25;
+    // const meiizyyyy = true; //boolean
+    // const meiizyyyy = undefined;
+    // const meiizyyyy = [1, 2, 3];
+    const meiizyyyy = {
+        name: "Duc",
+        age: 25,
+    };
     return (
-        //Fragment
+        //Fragment                      //Convert Stringify
         <>
-            <div>Hello World! with React</div>
+            <div>Hello World! with {JSON.stringify(meiizyyyy)}</div>
+            <div>{console.log("Hello meiizyyyy")}</div>
             <div className="child" style={{ fontSize: "50px" }}>
                 Hello World! with React Fragment
             </div>
