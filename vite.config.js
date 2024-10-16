5f04605b6b8bfa46896cc8c12f1013e752373ffd
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2024-10-16 18:04:04 +0700

    Hello World! and reconfig to open in chrome

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import dns from 'dns'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dns from "dns";
+import open, { apps } from "open";
 // https://vitejs.dev/config/server-options.html#server-options
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-})
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
+});
+await open("http://localhost:3000/", {
+	app: {
+		name: "chrome",
+		arguments: ["--user-data-dir=C:\\Users\\meiizyyyy\\AppData\\Local\\Google\\Chrome\\User Data", "--profile-directory=Profile 5"],
+	},
+});
