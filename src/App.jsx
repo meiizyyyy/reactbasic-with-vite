afc58586c7397e56a414ce4d2342cac95870d9a5
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2024-10-22 00:40:50 +0700

    #51 Index Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,17 @@
 import "./components/todo/todo.css";
-import TodoData from "./components/todo/TodoData";
-import TodoNew from "./components/todo/TodoNew";
-import reactLogo from "./assets/react.svg";
-import { useState } from "react";
+
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
 import { Outlet } from "react-router-dom";
 
+
+
 const App = () => {
-    // bien       sau khi chay cai nay se cap nhat ali bien
-    const [todoList, setTodoList] = useState([
-        // {
-        //     id: 1,
-        //     name: "Learning React",
-        // },
-        // {
-        //     id: 2,
-        //     name: "Watching Youtube",
-        // },
-    ]);
-    // const name = "meiizyyyy";
-    // const age = "21";
-    // const data = {
-    //     address: "Son Tay",
-    //     country: "Viet Nam",
-    // };
-    // {key:value} => object
-
-    const addNewTodo = (name) => {
-        const newTodo = {
-            id: randomIntFromInterval(1, 99999999),
-            name: name,
-        };
-        // array.push
-        // todoList.push(newTodo);
-        //khong nen thao tac truc tiep voi State
-        setTodoList([...todoList, newTodo]);
-    };
-
-    const deleteTodo = (id) => {
-        console.log(id);
-        const newTodo = todoList.filter((item) => item.id !== id);
-        setTodoList(newTodo);
-    };
-
-    //Ham random
-    const randomIntFromInterval = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-
     // addNewTodo();
     return (
         <>
             <Header />
-            <div className="todo-container">
-                <div className="todo-title">Todo List</div>
-                <TodoNew addNewTodo={addNewTodo} />
-                {/* Truyền dữ liệu từ cha sang con */}
-
-                {/* dieu kien  */}
-                {/* {todoList.length > 0 && (
-                    <>
-                        <TodoData
-                            // name={name} age={age} data={data} address={data.address}
-                            todoList={todoList}
-                        />
-                    </>
-                )}
-                {todoList.length === 0 && (
-                    <div className="todo-image">
-                        <img src={reactLogo} className="logo" />
-                    </div>
-                )} */}
-                {/* Toan tu 3 ngoi */}
-                {todoList.length > 0 ? (
-                    <>
-                        <TodoData
-                            // name={name} age={age} data={data} address={data.address}
-                            todoList={todoList}
-                            deleteTodo={deleteTodo}
-                        />
-                    </>
-                ) : (
-                    <div className="todo-image">
-                        <img src={reactLogo} className="logo" />
-                    </div>
-                )}
-            </div>
             <Outlet />
-
             <Footer />
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,17 @@ import LoginPage from "./pages/login.jsx";
 import ProductsPage from "./pages/products.jsx";
 import UsersPage from "./pages/users.jsx";
 import RegisterPage from "./pages/register.jsx";
+import TodoApp from "./components/todo/TodoApp.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
+            {
+                index: true,
+                element: <TodoApp />,
+            },
             {
                 path: "/users",
                 element: <UsersPage />,
