16450c52721e136f5019467a9945434d8427e9a5
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2024-11-03 03:35:04 +0700

    #82 Show avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,11 +1,12 @@
-import React, {  } from "react";
-import { Drawer } from "antd";
+import React from "react";
+import { Drawer, Button } from "antd";
 const ViewUserDetail = (props) => {
     const { dataDetail, setDataDetail, isUserDetailOpen, setIsUserDetailOpen } = props;
 
     return (
         <>
             <Drawer
+                width={"30vw"}
                 title="User Detail"
                 open={isUserDetailOpen}
                 onClose={() => {
@@ -15,6 +16,26 @@ const ViewUserDetail = (props) => {
                 {dataDetail ? (
                     <>
                         <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
+                            <div>
+                                <p>Avatar: </p>
+                                <img height={"250px"} width={"250px"} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} alt="" />
+                                <div>
+                                    <label
+                                        htmlFor="btn-upload"
+                                        style={{
+                                            display: "block",
+                                            width: "fit-content",
+                                            marginTop: "15px",
+                                            padding: "10px",
+                                            background: "orange",
+                                            borderRadius: "5px",
+                                            cursor: "pointer",
+                                        }}>
+                                        Upload Avatar
+                                    </label>
+                                    <input type="file" name="" id="btn-upload" hidden />
+                                </div>
+                            </div>
                             <div>
                                 <span>ID: {dataDetail._id}</span>
                             </div>
